8dbe1fe42d200a88fbfb935a0ed05e275722e016
António Capela <acapela96@hotmail.com>
2023-04-29 12:56:22 +0100

    2022 day 1

---
+++ 2022/day1/src/main.rs
@@ -0,0 +1,38 @@
+fn main() {
+    answer();
+}
+
+fn answer() {
+    let data = std::fs::read_to_string("input").expect("Failed to read file");
+
+    let mut calories = parse_data(data);
+    
+    calories.sort_unstable();
+
+    let maximum = calories.last().expect("should not be empty");
+    let total: i32 = calories.iter().rev().take(3).sum();
+
+    println!("maximum: {}", maximum);
+    println!("top3: {}", total);
+}
+
+fn parse_data(data: String) -> Vec<i32> {
+    let elves_calories = data.split("\n\n");
+
+    let elves_total_calories = elves_calories.map(|elf_calories| {
+        elf_calories
+            .split('\n')
+            .map(|value| {
+                if value.is_empty() {
+                    0
+                } else {
+                    value
+                        .parse::<i32>()
+                        .expect("failed to parse calories as int")
+                }
+            })
+            .sum::<i32>()
+    });
+
+    elves_total_calories.collect::<Vec<_>>()
+}
