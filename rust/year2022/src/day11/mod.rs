8db8c5623ba9b5bae048bc9b992207aca845ca00
António Capela <acapela96@hotmail.com>
2023-05-22 20:11:09 +0100

    improve code readability(?) This is done by creating a abstracting keeping track or remainders into the class RemainStorage

--- rust/year2022/src/day11/mod.rs
+++ rust/year2022/src/day11/mod.rs
@@ -1,67 +1,108 @@
 use itertools::Itertools;
 use std::collections::{HashMap, VecDeque};
 
-// Maybe do this
-struct RemainStorage {
+struct NumberRemainder {
     storage: HashMap<i32, i32>,
 }
 
-struct Monkey {
-    items: VecDeque<i32>,
-    operation: Box<dyn Fn(i32) -> i32>,
-    test_divisible_by: i32,
-    true_condition_id: usize,
-    false_condition_id: usize,
-}
-
-struct Monkey2 {
-    items: VecDeque<HashMap<i32, i32>>,
-    operation_remainder: Box<dyn Fn(HashMap<i32, i32>) -> HashMap<i32, i32>>,
-    test_divisible_by: i32,
-    true_condition_id: usize,
-    false_condition_id: usize,
-}
-
-impl std::fmt::Debug for Monkey {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        f.debug_struct("Monkey")
-            .field("items", &self.items)
-            .field("test_divisible_by", &self.test_divisible_by)
-            .field("true_condition_id", &self.true_condition_id)
-            .field("false_condition_id", &self.false_condition_id)
-            .finish()
+/// Stores the remainder of a number in a hashmap for a given set of denominators.
+impl NumberRemainder {
+    fn from_i32(i: &i32, denominators: &[i32]) -> Self {
+        let mut storage = HashMap::new();
+        for denominator in denominators {
+            storage.insert(*denominator, i % denominator);
+        }
+        Self { storage }
     }
+
+    fn is_divisible_by(&self, denominator: &i32) -> bool {
+        self.storage[denominator] == 0
+    }
+
+    fn add_i32(&self, other: &i32) -> Self {
+        Self {
+            storage: self
+                .storage
+                .iter()
+                .map(|(k, v)| (*k, (v + other) % k))
+                .collect(),
+        }
+    }
+
+    fn add_number_remainder(&self, other: &Self) -> Self {
+        Self {
+            storage: self
+                .storage
+                .keys()
+                .map(|k| (*k, (self.storage[k] + other.storage[k]) % k))
+                .collect(),
+        }
+    }
+
+    fn mul_i32(&self, other: &i32) -> Self {
+        Self {
+            storage: self
+                .storage
+                .iter()
+                .map(|(k, v)| (*k, (v * other) % k))
+                .collect(),
+        }
+    }
+
+    fn mul_number_remainder(&self, other: &Self) -> Self {
+        Self {
+            storage: self
+                .storage
+                .keys()
+                .map(|k| (*k, (self.storage[k] * other.storage[k]) % k))
+                .collect(),
+        }
+    }
+}
+
+type Monkey1Operation = Box<dyn Fn(i32) -> i32>;
+struct Monkey1 {
+    items: VecDeque<i32>,
+    operation: Monkey1Operation,
+    test_divisible_by: i32,
+    true_condition_id: usize,
+    false_condition_id: usize,
+}
+
+type Monkey2Operation = Box<dyn Fn(NumberRemainder) -> NumberRemainder>;
+struct Monkey2 {
+    items: VecDeque<NumberRemainder>,
+    operation_remainder: Monkey2Operation,
+    test_divisible_by: i32,
+    true_condition_id: usize,
+    false_condition_id: usize,
 }
 
 pub fn answer() {
     let data =
         std::fs::read_to_string("year2022/src/day11/input.txt").expect("Unable to read file");
 
-    let (mut monkeys, mut monkeys2) = parse_data(&data);
+    let (monkeys, monkeys2) = parse_data(&data);
 
     let monkey_business = answer1(monkeys);
-    let monkey_business2 = answer2(monkeys2);
-
     println!("{:?}", monkey_business);
+
+    let monkey_business2 = answer2(monkeys2);
     println!("{:?}", monkey_business2);
 }
 
-fn answer1(mut monkeys: Vec<Monkey>) -> usize {
-    let counts = inspect_count(&mut monkeys);
+fn answer1(mut monkeys: Vec<Monkey1>) -> usize {
+    let counts = inspect_count1(&mut monkeys);
 
-    let monkey_business = counts.iter().sorted().rev().take(2).product::<usize>();
-    monkey_business
-}
-fn answer2(mut monkeys: Vec<Monkey2>) -> usize {
-    let counts = inspect_count2(&mut monkeys);
-
-    dbg!(&counts);
-
-    let monkey_business = counts.iter().sorted().rev().take(2).product::<usize>();
+    let monkey_business = counts
+        .into_iter()
+        .sorted_by(|a, b| b.cmp(a))
+        .take(2)
+        .product::<usize>();
     monkey_business
 }
 
-fn inspect_count(monkeys: &mut Vec<Monkey>) -> Vec<usize> {
+fn inspect_count1(monkeys: &mut Vec<Monkey1>) -> Vec<usize> {
     let mut count = vec![0; monkeys.len()];
 
     for _round in 0..20 {
@@ -78,11 +119,22 @@ fn inspect_count(monkeys: &mut Vec<Monkey>) -> Vec<usize> {
                 monkeys[id_to].items.push_back(new_item);
             }
         }
-        dbg!(&monkeys);
     }
 
     count
 }
+
+fn answer2(mut monkeys: Vec<Monkey2>) -> usize {
+    let counts = inspect_count2(&mut monkeys);
+
+    let monkey_business = counts
+        .into_iter()
+        .sorted_by(|a, b| b.cmp(a))
+        .take(2)
+        .product::<usize>();
+    monkey_business
+}
+
 fn inspect_count2(monkeys: &mut Vec<Monkey2>) -> Vec<usize> {
     let mut count = vec![0; monkeys.len()];
 
@@ -92,7 +144,7 @@ fn inspect_count2(monkeys: &mut Vec<Monkey2>) -> Vec<usize> {
                 count[monkey_id] += 1;
 
                 let new_item = (monkeys[monkey_id].operation_remainder)(item);
-                let id_to = if *new_item.get(&monkeys[monkey_id].test_divisible_by).unwrap() == 0 {
+                let id_to = if new_item.is_divisible_by(&monkeys[monkey_id].test_divisible_by) {
                     monkeys[monkey_id].true_condition_id
                 } else {
                     monkeys[monkey_id].false_condition_id
@@ -105,7 +157,7 @@ fn inspect_count2(monkeys: &mut Vec<Monkey2>) -> Vec<usize> {
     count
 }
 
-fn parse_data(data: &str) -> (Vec<Monkey>, Vec<Monkey2>) {
+fn parse_data(data: &str) -> (Vec<Monkey1>, Vec<Monkey2>) {
     let denominators = data
         .lines()
         .skip(3)
@@ -131,21 +183,21 @@ fn parse_data(data: &str) -> (Vec<Monkey>, Vec<Monkey2>) {
                 .map(|item| item.parse().unwrap())
                 .collect();
 
-            //   Operation: new = old + 6
+            //   Operation: new = old {op} {other}
             let (operation, operation_remainder) =
                 parse_operation(&chunk.next().unwrap()["  Operation: new = old ".len()..]);
 
-            //   Test: divisible by 3
+            //   Test: divisible by {num}
             let test_divisible_by = chunk.next().unwrap()["  Test: divisible by ".len()..]
                 .parse()
                 .unwrap();
 
-            //     If true: throw to monkey 2
+            //     If true: throw to monkey {id}
             let true_condition_id = chunk.next().unwrap()["    If true: throw to monkey ".len()..]
                 .parse()
                 .unwrap();
 
-            //     If false: throw to monkey 3
+            //     If false: throw to monkey {id}
             let false_condition_id = chunk.next().unwrap()
                 ["    If false: throw to monkey ".len()..]
                 .parse()
@@ -153,10 +205,10 @@ fn parse_data(data: &str) -> (Vec<Monkey>, Vec<Monkey2>) {
 
             let items2 = items
                 .iter()
-                .map(|item| item_to_remainder(item, &denominators))
+                .map(|item| NumberRemainder::from_i32(item, &denominators))
                 .collect();
             (
-                Monkey {
+                Monkey1 {
                     items,
                     operation,
                     test_divisible_by,
@@ -177,20 +229,7 @@ fn parse_data(data: &str) -> (Vec<Monkey>, Vec<Monkey2>) {
     (monkeys, monkeys2)
 }
 
-fn item_to_remainder(item: &i32, denominators: &[i32]) -> HashMap<i32, i32> {
-    let mut remainder = HashMap::new();
-    for denominator in denominators {
-        remainder.insert(*denominator, item % denominator);
-    }
-    remainder
-}
-
-fn parse_operation(
-    expression: &str,
-) -> (
-    Box<dyn Fn(i32) -> i32>,
-    Box<dyn Fn(HashMap<i32, i32>) -> HashMap<i32, i32>>,
-) {
+fn parse_operation(expression: &str) -> (Monkey1Operation, Monkey2Operation) {
     let mut iter = expression.split_whitespace();
     let op = iter.next().unwrap();
     let y = iter.next().unwrap();
@@ -199,15 +238,11 @@ fn parse_operation(
         match op {
             "+" => (
                 Box::new(|x| x + x),
-                Box::new(move |remainder| {
-                    remainder.into_iter().map(|(k, v)| (k, (v + v) % k)).collect()
-                }),
+                Box::new(move |remainder| remainder.add_number_remainder(&remainder)),
             ),
             "*" => (
                 Box::new(|x| x * x),
-                Box::new(move |remainder| {
-                    remainder.into_iter().map(|(k, v)| (k, (v * v) % k)).collect()
-                }),
+                Box::new(move |remainder| remainder.mul_number_remainder(&remainder)),
             ),
             _ => unreachable!(),
         }
@@ -216,15 +251,11 @@ fn parse_operation(
         match op {
             "+" => (
                 Box::new(move |x| x + y),
-                Box::new(move |remainder| {
-                    remainder.into_iter().map(|(k, v)| (k, (v + y) % k)).collect()
-                }),
+                Box::new(move |remainder| remainder.add_i32(&y)),
             ),
             "*" => (
                 Box::new(move |x| x * y),
-                Box::new(move |remainder| {
-                    remainder.into_iter().map(|(k, v)| (k, (v * y) % k)).collect()
-                }),
+                Box::new(move |remainder| remainder.mul_i32(&y)),
             ),
             _ => unreachable!(),
         }
