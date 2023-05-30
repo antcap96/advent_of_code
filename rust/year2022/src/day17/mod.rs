e08cbf196229c85b033042beb2354d66569e9174
António Capela <acapela96@hotmail.com>
2023-05-30 22:12:54 +0100

    still not a good solution but a bit better

--- rust/year2022/src/day17/mod.rs
+++ rust/year2022/src/day17/mod.rs
@@ -1,5 +1,6 @@
-use std::collections::HashSet;
-// TOO LOW: 1567599067609
+use std::collections::{HashMap, HashSet};
+
+#[must_use = "iterator adaptors are lazy and do nothing unless consumed"]
 struct CycleEnumerate<I> {
     orig: I,
     iter: I,
@@ -114,69 +115,119 @@ impl Chamber {
             .map(|y| y + 1)
             .unwrap_or(0)
     }
+}
 
-    fn show(&self) {
-        for i in 0..self.height() {
-            for j in 0..7 {
-                let point = Point { x: j, y: i };
-                if self.stopped_rocks.contains(&point) {
-                    print!("#");
-                } else {
-                    print!(".");
-                }
-            }
-            println!();
+struct Answer {
+    init: Vec<i64>,
+    cycle: Vec<i64>,
+}
+
+impl Answer {
+    fn new(mut heights: Vec<Vec<i64>>) -> Answer {
+        heights.pop();
+        let mut cycle = heights.pop().unwrap();
+        let min = *cycle.iter().min().unwrap();
+        cycle.iter_mut().for_each(|x| *x -= min);
+
+        let init = heights.into_iter().flatten().collect::<Vec<_>>();
+
+        Answer { init, cycle }
+    }
+
+    fn of(&self, n_rocks: i64) -> i64 {
+        if n_rocks < self.init.len() as i64 {
+            self.init[n_rocks as usize]
+        } else {
+            let cycle_len = self.cycle.len() as i64 - 1;
+            let init_len = self.init.len() - 1;
+            let cycle_idx = (n_rocks - init_len as i64) % cycle_len;
+            let cycle_n = (n_rocks - init_len as i64) / cycle_len;
+            self.init.last().unwrap()
+                + self.cycle[cycle_idx as usize]
+                + cycle_n * self.cycle.last().unwrap()
         }
-        println!();
     }
 }
 
-fn fall_piece<'a>(
-    chamber: &mut Chamber,
-    piece: &Piece,
-    jet_stream: &mut impl Iterator<Item = &'a Direction>,
-) {
-    let position = chamber.start_position();
+fn find_pattern(mut chamber: Chamber, pieces: &[Piece], jet_stream: &[Direction]) -> Answer {
+    // [cycle][index]
+    let mut heights: Vec<Vec<i64>> = vec![vec![0]];
 
-    let mut falling_piece = FallingPiece { piece, position };
+    let mut jet_stream_iter = jet_stream.iter().cycle_enumerate();
+    let piece_iter = pieces.iter().cycle();
 
-    for direction in jet_stream {
-        // go left or right
-        let next_position = falling_piece.position.add(&direction.as_offset());
+    for piece in piece_iter {
+        let position = chamber.start_position();
 
-        let next_falling_piece = FallingPiece {
-            piece: falling_piece.piece,
-            position: next_position,
-        };
+        let mut falling_piece = FallingPiece { piece, position };
 
-        if next_falling_piece
-            .iter_points()
-            .all(|p| chamber.available(&p))
-        {
-            falling_piece = next_falling_piece;
+        let mut end_idx = 0;
+        for (i, direction) in &mut jet_stream_iter {
+            // go left or right
+            let next_position = falling_piece.position.add(&direction.as_offset());
+
+            let next_falling_piece = FallingPiece {
+                piece: falling_piece.piece,
+                position: next_position,
+            };
+
+            if next_falling_piece
+                .iter_points()
+                .all(|p| chamber.available(&p))
+            {
+                falling_piece = next_falling_piece;
+            }
+
+            // go down
+            let next_position = falling_piece.position.add(&Offset { x: 0, y: -1 });
+
+            let next_falling_piece = FallingPiece {
+                piece: falling_piece.piece,
+                position: next_position,
+            };
+
+            if next_falling_piece
+                .iter_points()
+                .all(|p| chamber.available(&p))
+            {
+                falling_piece = next_falling_piece;
+            } else {
+                end_idx = i;
+                break;
+            }
         }
 
-        // go down
-        let next_position = falling_piece.position.add(&Offset { x: 0, y: -1 });
+        for point in falling_piece.iter_points() {
+            chamber.stopped_rocks.insert(point);
+        }
 
-        let next_falling_piece = FallingPiece {
-            piece: falling_piece.piece,
-            position: next_position,
-        };
+        if end_idx >= heights.len() {
+            if heights.len() >= 2 {
+                let last = &heights[end_idx - 1];
+                let last_min = *last.iter().min().unwrap();
+                let last = last.iter().map(|x| x - last_min).collect::<Vec<_>>();
+                let prev_last = &heights[end_idx - 2];
+                let prev_last_min = *prev_last.iter().min().unwrap();
+                let prev_last = prev_last
+                    .iter()
+                    .map(|x| x - prev_last_min)
+                    .collect::<Vec<_>>();
 
-        if next_falling_piece
-            .iter_points()
-            .all(|p| chamber.available(&p))
-        {
-            falling_piece = next_falling_piece;
+                dbg!(last.last());
+                dbg!(prev_last.last());
+                if last == prev_last {
+                    break;
+                }
+            }
+            heights.push(vec![
+                *heights[end_idx - 1].last().unwrap(),
+                chamber.height() as i64,
+            ]);
         } else {
-            break;
+            heights[end_idx].push(chamber.height() as i64);
         }
     }
-
-    for point in falling_piece.iter_points() {
-        chamber.stopped_rocks.insert(point);
-    }
+    Answer::new(heights)
 }
 
 pub fn answer() {
@@ -184,6 +235,18 @@ pub fn answer() {
         std::fs::read_to_string("year2022/src/day17/input.txt").expect("Failed to read file");
 
     let jet_stream = parse_data(&data);
+    let pieces = build_pieces();
+    let chamber = Chamber {
+        stopped_rocks: HashSet::new(),
+    };
+
+    let answer = find_pattern(chamber, &pieces, &jet_stream);
+
+    println!("Answer1: {}", answer.of(2022));
+    println!("Answer1: {}", answer.of(1000000000000));
+}
+
+fn build_pieces() -> Vec<Piece> {
     let pieces = vec![
         Piece {
             points: vec![
@@ -228,35 +291,7 @@ pub fn answer() {
             ],
         },
     ];
-
-    let pieces_iter = pieces.iter().cycle();
-    let mut jet_stream_iter = jet_stream.iter().cycle_enumerate().peekable();
-
-    let mut chamber = Chamber {
-        stopped_rocks: HashSet::new(),
-    };
-    let mut heights: Vec<i64> = vec![2762];
-
-    for piece in pieces_iter {
-        let mut fixme_temp_iter = (&mut jet_stream_iter).map(|(_, d)| d);
-        fall_piece(&mut chamber, piece, &mut fixme_temp_iter);
-        if jet_stream_iter.peek().unwrap().0 == 1 {
-            heights.push(chamber.height() as i64);
-        } else if jet_stream_iter.peek().unwrap().0 > 1{
-            break;
-        }
-    }
-    let min = *heights.iter().min().unwrap();
-    heights.iter_mut().for_each(|h| *h = *h - min);
-
-    let piece_count = heights.len() as i64 - 1;
-    let n = 1000000000000 - 1740;
-    let x = n % piece_count;
-    let y = n / piece_count;
-    let temp = 2762 + heights[x as usize] + y * heights.last().unwrap();
-    dbg!(x, y, temp, piece_count);
-
-    println!("The height of the chamber is {}", chamber.height());
+    pieces
 }
 
 fn parse_data(data: &str) -> Vec<Direction> {
@@ -268,4 +303,21 @@ fn parse_data(data: &str) -> Vec<Direction> {
             _ => panic!("Unexpected character"),
         })
         .collect()
+}
+
+// Failing
+#[test]
+fn test1() {
+    let jet_stream = parse_data(">>><<><>><<<>><>>><<<>>><<<><<<>><>><<>>");
+    let pieces = build_pieces();
+    let chamber = Chamber {
+        stopped_rocks: HashSet::new(),
+    };
+
+    let answer = find_pattern(chamber, &pieces, &jet_stream);
+
+    
+    assert!(answer.of(2022) == 3068);
+    assert!(answer.of(1000000000000) == 1514285714288);
+
 }
