5cc68a587f1ffe41a1f356a9962404136bcb5226
António Capela <acapela96@hotmail.com>
2023-05-30 20:13:51 +0100

    disgusting way to get the answer for 2022 day17

--- rust/year2022/src/day17/mod.rs
+++ rust/year2022/src/day17/mod.rs
@@ -1,4 +1,39 @@
 use std::collections::HashSet;
+// TOO LOW: 1567599067609
+struct CycleEnumerate<I> {
+    orig: I,
+    iter: I,
+    count: usize,
+}
+
+impl<I: Iterator + Clone> Iterator for CycleEnumerate<I> {
+    type Item = (usize, <I as Iterator>::Item);
+
+    fn next(&mut self) -> Option<Self::Item> {
+        match self.iter.next() {
+            None => {
+                self.iter = self.orig.clone();
+                self.count += 1;
+                self.iter.next().map(|x| (self.count, x))
+            }
+            y => y.map(|x| (self.count, x)),
+        }
+    }
+}
+
+trait CycleEnumerateExt: Iterator + Sized {
+    fn cycle_enumerate(self) -> CycleEnumerate<Self>;
+}
+
+impl<I: Iterator + Clone> CycleEnumerateExt for I {
+    fn cycle_enumerate(self) -> CycleEnumerate<I> {
+        CycleEnumerate {
+            orig: self.clone(),
+            iter: self,
+            count: 0,
+        }
+    }
+}
 
 #[derive(Debug)]
 enum Direction {
@@ -102,7 +137,6 @@ fn fall_piece<'a>(
     jet_stream: &mut impl Iterator<Item = &'a Direction>,
 ) {
     let position = chamber.start_position();
-    let start_y = position.y;
 
     let mut falling_piece = FallingPiece { piece, position };
 
@@ -139,12 +173,9 @@ fn fall_piece<'a>(
             break;
         }
     }
+
     for point in falling_piece.iter_points() {
         chamber.stopped_rocks.insert(point);
-    }
-
-    if falling_piece.piece.points.contains(&Offset { x: 3, y: 0 }) {
-        dbg!(start_y - falling_piece.iter_points().map(|x| x.y).min().unwrap_or(0));
     }
 }
 
@@ -198,16 +229,33 @@ pub fn answer() {
         },
     ];
 
-    let pieces_iter = pieces.iter().cycle().take(2022);
-    let mut jet_stream_iter = jet_stream.iter().cycle();
+    let pieces_iter = pieces.iter().cycle();
+    let mut jet_stream_iter = jet_stream.iter().cycle_enumerate().peekable();
 
     let mut chamber = Chamber {
         stopped_rocks: HashSet::new(),
     };
+    let mut heights: Vec<i64> = vec![2762];
 
     for piece in pieces_iter {
-        fall_piece(&mut chamber, piece, &mut jet_stream_iter)
+        let mut fixme_temp_iter = (&mut jet_stream_iter).map(|(_, d)| d);
+        fall_piece(&mut chamber, piece, &mut fixme_temp_iter);
+        if jet_stream_iter.peek().unwrap().0 == 1 {
+            heights.push(chamber.height() as i64);
+        } else if jet_stream_iter.peek().unwrap().0 > 1{
+            break;
+        }
     }
+    let min = *heights.iter().min().unwrap();
+    heights.iter_mut().for_each(|h| *h = *h - min);
+
+    let piece_count = heights.len() as i64 - 1;
+    let n = 1000000000000 - 1740;
+    let x = n % piece_count;
+    let y = n / piece_count;
+    let temp = 2762 + heights[x as usize] + y * heights.last().unwrap();
+    dbg!(x, y, temp, piece_count);
+
     println!("The height of the chamber is {}", chamber.height());
 }
 
