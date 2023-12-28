faf691f0986ca7a4c27752ffcaf434aaaf45bb14
António Capela <acapela96@hotmail.com>
2023-12-28 13:12:07 +0000

    Pluto external Project.toml

--- julia/AdventOfCode/src/2023/day11/answer_sum.jl
+++ julia/AdventOfCode/src/2023/day11/answer_sum.jl
@@ -4,6 +4,13 @@
 using Markdown
 using InteractiveUtils
 
+# ╔═╡ 804dc592-cc75-49ca-91c1-ceb67d765c56
+begin
+    using Pkg
+    Pkg.activate(Base.current_project())
+    Pkg.instantiate()
+end
+
 # ╔═╡ 68b82337-ea65-4091-872c-7f51dfd826e9
 using Test
 
@@ -26,12 +33,12 @@ rev_cumsum(arr) = (cumsum(arr |> Iterators.reverse) |> reverse!)
 
 # ╔═╡ e8ebceb9-73bd-43b0-ac6f-3623722f63af
 function answer_(input, expansion_factor)
-	sum(1:2) do axis
-		n_galaxies = dropdims(sum(input; dims=axis); dims=axis)
-		times_traveled = (cumsum(n_galaxies) .- n_galaxies) .* rev_cumsum(n_galaxies)
-		weights = ((expansion_factor - 1) .* (n_galaxies .== 0) .+ 1)
-		weights' * times_traveled
-	end
+    sum(1:2) do axis
+        n_galaxies = dropdims(sum(input; dims=axis); dims=axis)
+        times_traveled = (cumsum(n_galaxies) .- n_galaxies) .* rev_cumsum(n_galaxies)
+        weights = ((expansion_factor - 1) .* (n_galaxies .== 0) .+ 1)
+        weights' * times_traveled
+    end
 end
 
 # ╔═╡ a9d50b91-a713-4619-815f-3196030832f3
@@ -81,51 +88,8 @@ test_input_1 = "...#......
 # ╔═╡ 890ca4e4-e62f-4fc1-93a7-ea4aca2e6b11
 @test answer_(test_input_1 |> parse_input, 100) == 8410
 
-# ╔═╡ 00000000-0000-0000-0000-000000000001
-PLUTO_PROJECT_TOML_CONTENTS = """
-[deps]
-Test = "8dfed614-e22c-5e08-85e1-65c5234f0b40"
-"""
-
-# ╔═╡ 00000000-0000-0000-0000-000000000002
-PLUTO_MANIFEST_TOML_CONTENTS = """
-# This file is machine-generated - editing it directly is not advised
-
-julia_version = "1.9.4"
-manifest_format = "2.0"
-project_hash = "71d91126b5a1fb1020e1098d9d492de2a4438fd2"
-
-[[deps.Base64]]
-uuid = "2a0f44e3-6c83-55bd-87e4-b1978d98bd5f"
-
-[[deps.InteractiveUtils]]
-deps = ["Markdown"]
-uuid = "b77e0a4c-d291-57a0-90e8-8db25a27a240"
-
-[[deps.Logging]]
-uuid = "56ddb016-857b-54e1-b83d-db4d58db5568"
-
-[[deps.Markdown]]
-deps = ["Base64"]
-uuid = "d6f4376e-aef5-505a-96c1-9c027394607a"
-
-[[deps.Random]]
-deps = ["SHA", "Serialization"]
-uuid = "9a3f8284-a2c9-5f02-9a11-845980a1fd5c"
-
-[[deps.SHA]]
-uuid = "ea8e919c-243c-51af-8825-aaa63cd721ce"
-version = "0.7.0"
-
-[[deps.Serialization]]
-uuid = "9e88b42a-f829-5b0c-bbe9-9e923198166b"
-
-[[deps.Test]]
-deps = ["InteractiveUtils", "Logging", "Random", "Serialization"]
-uuid = "8dfed614-e22c-5e08-85e1-65c5234f0b40"
-"""
-
 # ╔═╡ Cell order:
+# ╠═804dc592-cc75-49ca-91c1-ceb67d765c56
 # ╠═42d0b1b6-981a-11ee-0a01-6f7d5b828f97
 # ╠═37725a67-02b1-45d6-b4b9-3e110aa03f8b
 # ╠═c8ab9b7c-98d1-4e89-b9da-5ac181c6e5fb
@@ -140,5 +104,3 @@ uuid = "8dfed614-e22c-5e08-85e1-65c5234f0b40"
 # ╠═ef19a91f-4eee-4c4e-bc4a-dd8d38f299b1
 # ╠═68b7b111-21c6-4960-83a0-47045ebecde8
 # ╠═890ca4e4-e62f-4fc1-93a7-ea4aca2e6b11
-# ╟─00000000-0000-0000-0000-000000000001
-# ╟─00000000-0000-0000-0000-000000000002
