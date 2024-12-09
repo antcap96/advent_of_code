97ac9461e2cf4d0020263b80aa74b934a3925d60
António Capela <acapela96@hotmail.com>
2024-12-09 22:53:12 +0000

    use list instead of linked list

--- python/year2024/year2024/day9.py
+++ python/year2024/year2024/day9.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
+from dataclasses import dataclass
 import itertools
-from typing import Iterable
 
 from year2024.utils.aoc import Solution
 
@@ -12,7 +12,7 @@ def parse_input(string: str) -> list[int]:
 def calculate_answer1(numbers: list[int]) -> int:
     length = sum(numbers)
 
-    nums: list[int | None] = [None for i in range(length)]
+    nums: list[int | None] = [None for _ in range(length)]
 
     at = 0
     for i, count in enumerate(numbers):
@@ -34,127 +34,74 @@ def calculate_answer1(numbers: list[int]) -> int:
     return sum(i * x for i, x in enumerate(nums) if x is not None)
 
 
-class LinkedListItem[T]:
-    def __init__(
-        self,
-        value: T,
-        next: LinkedListItem[T] | None,
-        prev: LinkedListItem[T] | None,
-    ) -> None:
-        self.next = next
-        self.prev = prev
-        self.value = value
+def count(sections: list[Section]) -> int:
+    def expand(x: Section) -> itertools.repeat[int | None]:
+        return itertools.repeat(x.id_number, x.size)
 
-    @staticmethod
-    def from_iterable[U](
-        items: Iterable[U],
-    ) -> tuple[LinkedListItem[U], LinkedListItem[U]]:
-        items_iter = iter(items)
-        first = LinkedListItem(next(items_iter), None, None)
-        last = first
-        for item in items_iter:
-            last = last.insert(item)
-
-        return (first, last)
-
-    def insert(self, value: T) -> LinkedListItem[T]:
-        next = LinkedListItem(value, self.next, self)
-        if self.next is not None:
-            self.next.prev = next
-        self.next = next
-
-        return next
-
-    def insert_before(self, value: T) -> LinkedListItem[T]:
-        prev = LinkedListItem(value, next=self, prev=self.prev)
-        if self.prev is not None:
-            self.prev.next = prev
-        self.prev = prev
-
-        return prev
-
-    def replace(self, value: T) -> LinkedListItem[T]:
-        self.value = value
-
-        return self
-
-    def delete(self) -> None:
-        if self.next is not None:
-            self.next.prev = self.prev
-        if self.prev is not None:
-            self.prev.next = self.next
-
-    def __iter__(self) -> LinkedListIter[T]:
-        return LinkedListIter(self)
-
-
-class LinkedListIter[T]:
-    def __init__(self, linked_list: LinkedListItem[T]) -> None:
-        self.linked_list = linked_list
-
-    def __iter__(self) -> LinkedListIter[T]:
-        return self
-
-    def __next__(self) -> T:
-        if self.linked_list is not None:
-            value = self.linked_list.value
-            self.linked_list = self.linked_list.next
-            return value
-        else:
-            raise StopIteration
-
-
-def count(id_and_count: LinkedListItem[tuple[int | None, int]]) -> int:
-    def expand(x: tuple[int | None, int]) -> itertools.repeat[int | None]:
-        return itertools.repeat(x[0], x[1])
-
-    nums = enumerate(itertools.chain(*map(expand, id_and_count)))
+    nums = enumerate(itertools.chain(*map(expand, sections)))
 
     return sum(i * x for i, x in nums if x is not None)
 
 
+@dataclass
+class Section:
+    id_number: int | None
+    size: int
+
+    @staticmethod
+    def empty():
+        return Section(None, 0)
+
+
+def expand_with_space(numbers: list[int]) -> list[Section]:
+    output: list[Section] = []
+
+    for i, number in enumerate(numbers):
+        if i % 2 == 0:
+            output.append(Section(i // 2, number))
+        else:
+            output.append(Section(None, number))
+            output.extend([Section.empty()] * (number - 1))
+
+    return output
+
+
 def calculate_answer2(numbers: list[int]) -> int:
-    id_and_count = [
-        (i // 2 if i % 2 == 0 else None, num) for i, num in enumerate(numbers)
-    ]
+    nums = expand_with_space(numbers)
 
-    (first, last) = LinkedListItem.from_iterable(id_and_count)
-    assert last.value[0] is not None
+    min_idx: list[None | int] = [0] * 10
 
-    min_idx: dict[int, None | LinkedListItem[tuple[int | None, int]]] = {
-        i: first for i in range(10)
-    }
-
-    current = last
-    while current is not None:
-        if current.value[0] is None:
-            current = current.prev
+    current = len(nums) - 1
+    while current >= 0:
+        if nums[current].id_number is None:
+            current -= 1
             continue
 
-        for key in min_idx:
-            if min_idx[key] == current:
-                min_idx[key] = None
+        i = min_idx[nums[current].size - 1]
+        while (
+            i is not None
+            and i < current
+            and not (nums[i].id_number is None and nums[i].size >= nums[current].size)
+        ):
+            i += 1
 
-        iter = min_idx[current.value[1]]
-        while iter is not None and iter != current:
-            iter = iter.next
-            assert iter is not None
-
-            if iter.value[0] is None and iter.value[1] >= current.value[1]:
-                break
-
-
-        if iter is not None and iter != current:
-            iter.replace((None, iter.value[1] - current.value[1]))
-            prev = iter.insert_before(current.value)
-            current.replace((None, current.value[1]))
-            min_idx[current.value[1]] = prev
+        if i is not None and i < current:
+            delta = nums[i].size - nums[current].size
+            if delta > 0:
+                assert nums[i + 1] == Section.empty()
+                nums[i + 1] = Section(None, delta)
+            nums[i] = nums[current]
+            nums[current] = Section(None, nums[current].size)
+            for j in range(nums[current].size - 1, 10):
+                existing = min_idx[j]
+                min_idx[j] = existing if existing is None or existing > i else i
         else:
-            min_idx[current.value[1]] = None
+            for j in range(nums[current].size - 1, 10):
+                min_idx[j] = None
 
-        current = current.prev
+        current -= 1
 
-    return count(first)
+    return count(nums)
 
 
 solution = Solution(parse_input, calculate_answer1, calculate_answer2, day=9)
